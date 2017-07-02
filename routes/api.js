f7f22041b450e69092f4e48a9860655dc4d70e2c
Vu Pham <phamanhvu101284@gmail.com>
2017-07-02 17:15:44 +1000

    integrate with ZoneController

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,27 @@
 var express = require('express')
 var router = express.Router()
+var ZoneController = require('../controllers/ZoneController')
 
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource
 
-  res.json({
-      confirmation: 'success',
-      resource: resource
-  })
+  if (resource == 'zone') {
+    ZoneController.find(req.query, function(err, results) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: err
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        results: results
+      })
+    })
+  }
 })
 
 module.exports = router
