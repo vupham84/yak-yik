add4b89d5b78fb501a9b562a05d8fa6694ffc3c7
Vu Pham <phamanhvu101284@gmail.com>
2017-07-02 15:14:51 +1000

    add mongoose model and update api

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var router = express.Router()
+
+router.get('/:resource', function(req, res, next) {
+  var resource = req.params.resource
+
+  res.json({
+      confirmation: 'success',
+      resource: resource
+  })
+})
+
+module.exports = router
